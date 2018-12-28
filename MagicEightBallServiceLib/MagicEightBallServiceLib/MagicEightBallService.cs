ed02e95553e7d33a2909cb128c2d82ebff833648
Oleksandr Shyian <ashqa14@gmail.com>
2018-12-28 23:31:39 +0200

    Add IEightBall interface

---
+++ MagicEightBallServiceLib/MagicEightBallServiceLib/MagicEightBallService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace MagicEightBallServiceLib
+{
+	public class MagicEightBallService : IEightBall
+	{
+		public MagicEightBallService()
+		{
+			Console.WriteLine("The 8-ball awaits your question...");
+		}
+
+		public string ObtainAnswerToQuestion(string userQuestion)
+		{
+			var answers = new string[] {"Future uncertain", "Yes", "No", "Hazy", "Ask again later", "Definitely"};
+			var random = new Random();
+			var answer = answers[random.Next(answers.Length)];
+			return answer;
+		}
+	}
+}
