aae91ff3e5f476bf8999f8656cdbc84584f859ee
Oleksandr Shyian <ashqa14@gmail.com>
2018-12-29 10:59:49 +0200

    Add DisplayHostInfo method

--- MagicEightBallServiceLib/MagicEightBallServiceHost/Program.cs
+++ MagicEightBallServiceLib/MagicEightBallServiceHost/Program.cs
@@ -1,27 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.ServiceModel;
-using System.Text;
-using System.Threading.Tasks;
+using System.ServiceModel.Description;
+
 using MagicEightBallServiceLib;
 
 namespace MagicEightBallServiceHost
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
 			Console.WriteLine("*** Console based WCF Host ***");
 
 			using (ServiceHost serviceHost = new ServiceHost(typeof(MagicEightBallService)))
 			{
 				serviceHost.Open();
+				DisplayHostInfo(serviceHost);
 
 				Console.WriteLine("The service is ready.");
 				Console.WriteLine("Press the Enter key to terminate service.");
 			}
+
 			Console.ReadLine();
+		}
+
+		private static void DisplayHostInfo(ServiceHost host)
+		{
+			Console.WriteLine();
+			Console.WriteLine("*** Host Info ***");
+
+			foreach (ServiceEndpoint serviceEndpoint in host.Description.Endpoints)
+			{
+				Console.WriteLine($"Address '{serviceEndpoint.Address}'");
+				Console.WriteLine($"Binding '{serviceEndpoint.Binding.Name}'");
+				Console.WriteLine($"Contract '{serviceEndpoint.Contract.Name}'");
+				Console.WriteLine("*******");
+			}
 		}
 	}
 }
